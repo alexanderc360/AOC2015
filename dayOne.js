9de3fb90fe98b051fc338529a0c601948eed811b
Alex Cohen <alexander.c.3600@gmail.com>
2020-12-29 22:25:09 -0500

    Day 1

---
+++ dayOne.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+let floor = 0;
+const data = fs.readFileSync('dayOne.txt', 'utf8')
+
+for (let i = 0; i < data.length; i++) {
+    if (data.charAt(i) === '(')
+        floor++;
+    else if (data.charAt(i) === ')')
+        floor--;
+    if (floor < 0) {
+        console.log(i + 1)
+        break;
+    }
+}
+
+// console.log(floor)
+
